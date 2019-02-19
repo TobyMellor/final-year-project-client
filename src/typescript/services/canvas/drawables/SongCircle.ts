b189155d966cfbb4637076be1873ec3097ad18ce
Toby Mellor <me@tobymellor.com>
2019-02-19 10:19:40 +0000

    feat: subtitle in songCircle #15

--- src/typescript/services/canvas/drawables/SongCircle.ts
+++ src/typescript/services/canvas/drawables/SongCircle.ts
@@ -8,6 +8,7 @@ import * as THREE from 'three';
 
 class SongCircle extends Updatable {
   private static BACKGROUND_COLOUR: number = 0xFFFFFF;
+  private static TEXT_COLOUR: number = 0xFFFFFF;
   public static EDGE_COLOUR: number = 0x000000;
   private static DARK_OVERLAY_OPACITY: number = 0.6;
   private static CIRCLE_RESOLUTION = 1;
@@ -101,41 +102,113 @@ class SongCircle extends Updatable {
     const loader = new THREE.FontLoader();
 
     loader.load('/dist/fonts/san-fransisco/regular.json', (font: any) => {
-      let fontSize = 0.5;
-      let predictedWidth: number = null;
-      let text: THREE.Mesh = null;
 
-      const generateText = (fontSize: number): THREE.Mesh => {
-        const geometry = new THREE.TextGeometry(track.name, {
+      /**
+       * Generate a text mesh with a font size.
+       *
+       * The text will aligned to the center of the circle horizontally.
+       *
+       * @param fontSize Size of the font
+       * @param text The text to be displayed
+       */
+      const generateText = (fontSize: number, text: string): THREE.Mesh => {
+        const geometry = new THREE.TextGeometry(text, {
           font,
           size: fontSize,
           height: 0,
           curveSegments: 4,
         });
-        geometry.center();
 
-        const material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF });
-        const text = new THREE.Mesh(geometry, material);
+        // Align the text to the center of the circle horizontally
+        function centerX(geometry: THREE.TextGeometry) {
+          const offset = new THREE.Vector3();
 
-        return text;
+          geometry.computeBoundingBox();
+          geometry.boundingBox.getCenter(offset).negate();
+
+          geometry.translate(offset.x, 0, 0);
+        }
+
+        // Ensure the MIDDLE of the text is aligned to the middle of the circle
+        // (instead of the LEFT of the text)
+        centerX(geometry);
+
+        const material = new THREE.MeshBasicMaterial({ color: SongCircle.TEXT_COLOUR });
+        const textMesh = new THREE.Mesh(geometry, material);
+
+        return textMesh;
       };
 
-      const circumference = radius * 2;
-      const padding = lineWidth * 4;
+      /**
+       * Creates and adds a text mesh to the scene that fits within the circle
+       *
+       * Since letters in text are varying, (e.g. m is wider than i), we need to
+       * try a large font size. If that doesn't fit within our size constraint,
+       * it will try again with a smaller font size until it fits.
+       *
+       * @param startFontSize The font size to start with
+       * @param maxWidth The width by which the text must not overflow
+       * @param text The text to be displayed
+       * @param isBelowCenter Whether the text will appear above or below the center
+       * @param centerPadding How much vertical padding should be applied
+       */
+      const addTextWithSizeConstraint = (
+        startFontSize: number,
+        maxWidth: number,
+        text: string,
+        isBelowCenter: boolean,
+        centerPadding: number,
+      ): { predictedSize: THREE.Vector3, fontSize: number } => {
+        let currentFontSize = startFontSize;
+        let predictedSize: THREE.Vector3 = null;
+        let textMesh: THREE.Mesh = null;
 
-      while (!predictedWidth || predictedWidth > circumference - padding) {
-        fontSize -= 0.025;
-        text = generateText(fontSize);
+        // Generate text with a font size. If it doesn't fit in the constraint, reduce and repeat.
+        while (!predictedSize || predictedSize.x > maxWidth) {
+          currentFontSize -= 0.025;
+          textMesh = generateText(currentFontSize, text);
 
-        const box = new THREE.Box3().setFromObject(text);
-        const center = new THREE.Vector3();
-        predictedWidth = box.getSize(center).x;
-      }
+          const box = new THREE.Box3().setFromObject(textMesh);
+          const center = new THREE.Vector3();
+          predictedSize = box.getSize(center);
+        }
 
-      super.addMesh({
-        mesh: text,
-        renderOrder: 2,
-      });
+        const position = WorldPoint.getPoint(0,
+                                             isBelowCenter ? -centerPadding - predictedSize.y
+                                                           : centerPadding,
+                                             0);
+
+        // Render this text
+        super.addMesh({
+          position,
+          mesh: textMesh,
+          renderOrder: 2,
+        });
+
+        return { predictedSize, fontSize: currentFontSize };
+      };
+
+      const circumference = radius * 2; // Circumference of the circle, excluding the lineWidth
+      const textHorizontalPadding = lineWidth * 5; // Horizontal padding to add to the constraint
+      const textVerticalPadding = 0.025; // Vertical padding away from the center
+
+      const titleMaxWidth = circumference - textHorizontalPadding;
+      const {
+        predictedSize: titlePredictedSize,
+        fontSize: titleFontSize,
+      } = addTextWithSizeConstraint(0.2,
+                                    titleMaxWidth,
+                                    track.name,
+                                    false,
+                                    textVerticalPadding);
+
+      const subtitleMaxWidth = titlePredictedSize.x;
+      addTextWithSizeConstraint(titleFontSize,
+                                subtitleMaxWidth,
+                                track.album.name,
+                                true,
+                                textVerticalPadding);
+
     });
   }
 
