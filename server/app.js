b42b2c04f919d251bfccc0ec7cf16529b96613b6
Vakar Eeralil <varghese@m5nv.com>
2019-05-14 21:20:51 +0200

    initial writing of login post method

--- server/app.js
+++ server/app.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const path = require("path");
 const dotenv = require("dotenv");
+const request = require("request");
+const cors = require("cors");
+const querystring = require("querystring");
+const cookieParser = require("cookie-parser");
+const randomString = require("randomstring");
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
+const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+const SPOTIFY_REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI;
+const stateKey = "spotify_auth_state";
 
-const app = express();
+const app = express()
+  .use("/dist", express.static(dist))
+  .use("/tracks", express.static(tracks))
+  .use(cors())
+  .use(cookieParser());
 const dist = path.join(__dirname, "..", "dist");
 const tracks = path.join(dist, "tracks");
 
@@ -15,8 +28,21 @@ app.get("/", (_, res) => {
   res.sendFile(dist + "/index.html");
 });
 
-app.use("/dist", express.static(dist));
-app.use("/tracks", express.static(tracks));
+app.post("/login", function(req, res) {
+  const state = randomString.generate(16);
+  const scope = "user-read-private user-read-email";
+  res.cookie(stateKey, state);
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state
+      })
+  );
+});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
