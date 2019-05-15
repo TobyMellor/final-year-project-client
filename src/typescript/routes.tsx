bfbe1d82190a09e7a843c1331bb84414faafb295
Vakar Eeralil <varghese@m5nv.com>
2019-05-16 00:43:25 +0200

    Test route for spotify authorization redirect

--- src/typescript/routes.tsx
+++ src/typescript/routes.tsx
@@ -2,9 +2,14 @@ import * as React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 
+const DemoComponent = () => (
+    <div>hello world</div>
+);
+
 const AppRoutes = () => (
     <Router>
         <Route path="/" exact component={LandingPage} />
+        <Route path="/spotify-authorization-redirect" exact component={DemoComponent} />
     </Router>
 );
 
