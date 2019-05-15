f51057ed9d78ac6593dfc8e0b344b03628f036ec
Vakar Eeralil <varghese@m5nv.com>
2019-05-15 23:51:06 +0200

    uncommenting

--- src/typescript/routes.tsx
+++ src/typescript/routes.tsx
@@ -1,11 +1,11 @@
-// import * as React from 'react';
-// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-// import LandingPage from './components/LandingPage';
+import * as React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
 
-// const AppRoutes = () => (
-//     <Router>
-//         <Route path="/" exact component={LandingPage} />
-//     </Router>
-// );
+const AppRoutes = () => (
+    <Router>
+        <Route path="/" exact component={LandingPage} />
+    </Router>
+);
 
-// export default AppRoutes;
+export default AppRoutes;
