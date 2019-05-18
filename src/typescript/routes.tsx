ee05c6fb22516ddaa0d5ed75a04a14f610b12432
Vakar Eeralil <varghese@m5nv.com>
2019-05-18 13:06:47 +0200

    Routes for login capture auth token and base route

--- src/typescript/routes.tsx
+++ src/typescript/routes.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import LandingPage from './components/LandingPage';
-
-const DemoComponent = () => (
-    <div>hello world</div>
-);
+import CheckAuthentication from './components/CheckAuthentication/CheckAuthentication';
+import LoginPage from './components/LoginPage/LoginPage';
+import CaptureAuthToken from './components/CaptureAuthToken/CaptureAuthToken';
 
 const AppRoutes = () => (
     <Router>
-        <Route path="/" exact component={LandingPage} />
-        <Route path="/spotify-authorization-redirect" exact component={DemoComponent} />
+        <Route path="/login" exact component={LoginPage} />
+        <Route path="/spotify-authorization-redirect" exact component={CaptureAuthToken} />
+        <Route path="/" component={CheckAuthentication} />
     </Router>
 );
 
