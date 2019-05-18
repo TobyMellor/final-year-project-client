0283a40eb9a4a3899c4c2ba6ab4a0a4085793e1d
Vakar Eeralil <varghese@m5nv.com>
2019-05-18 13:58:23 +0200

    Switch added

--- src/typescript/routes.tsx
+++ src/typescript/routes.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CheckAuthentication from './components/CheckAuthentication/CheckAuthentication';
 import CaptureAuthToken from './components/CaptureAuthToken/CaptureAuthToken';
 
 const AppRoutes = () => (
     <Router>
-        <Route path="/spotify-authorization-redirect" exact component={CaptureAuthToken} />
-        <Route path="/" component={CheckAuthentication} />
+        <Switch>
+            <Route path="/spotify-authorization-redirect" exact component={CaptureAuthToken} />
+            <Route path="/" component={CheckAuthentication} />
+        </Switch>
     </Router>
 );
 
