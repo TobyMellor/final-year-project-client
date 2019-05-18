540c174e732f1241253289f2bd1cd493e6b3e1f1
Vakar Eeralil <varghese@m5nv.com>
2019-05-18 13:21:12 +0200

    Removing login route

--- src/typescript/routes.tsx
+++ src/typescript/routes.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import CheckAuthentication from './components/CheckAuthentication/CheckAuthentication';
-import LoginPage from './components/LoginPage/LoginPage';
 import CaptureAuthToken from './components/CaptureAuthToken/CaptureAuthToken';
 
 const AppRoutes = () => (
     <Router>
-        <Route path="/login" exact component={LoginPage} />
         <Route path="/spotify-authorization-redirect" exact component={CaptureAuthToken} />
         <Route path="/" component={CheckAuthentication} />
     </Router>
