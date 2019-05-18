941974d2657fddf98013422c53eb3b53651faa07
Vakar Eeralil <varghese@m5nv.com>
2019-05-18 13:21:58 +0200

    Adding login page link

--- src/typescript/components/CheckAuthentication/CheckAuthentication.tsx
+++ src/typescript/components/CheckAuthentication/CheckAuthentication.tsx
@@ -5,6 +5,7 @@ import LandingPage from '../LandingPage/LandingPage';
 class CheckAuthentication extends React.Component {
   render() {
     return (
+        <a href="/login">go to login</a>
         <Route path="/" exact component={LandingPage} />
     );
   }
