1f05fdd00495a38935161a3460c3921e5758762c
Vakar Eeralil <varghese@m5nv.com>
2019-05-18 15:29:37 +0200

    login page design

--- src/typescript/components/LoginPage/LoginPage.tsx
+++ src/typescript/components/LoginPage/LoginPage.tsx
@@ -1,8 +1,21 @@
 import * as React from 'react';
 
 const LoginPage = () => (
-  <div>
-    <a href="login">Login to app using spotify</a>
+  <div className="container-fluid">
+    <div className="row align-items-center justify-content-center login-page">
+      <div className="col col-md-6 col-lg-4 align-self-center">
+        <div className="card login-card">
+          <div className="card-body">
+            <h3 className="card-title text-center">
+              Login with Spotify
+            </h3>
+            <div className="card-text">
+              <a className="btn btn-success btn-block btn-lg login-button" href="/login">Login</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
