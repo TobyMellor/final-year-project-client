634a47d61df9bcfa08b1adbab091f6299b53c01d
Vakar Eeralil <varghese@m5nv.com>
2019-05-18 14:43:01 +0200

    integrating login page component

--- src/typescript/components/CheckAuthentication/CheckAuthentication.tsx
+++ src/typescript/components/CheckAuthentication/CheckAuthentication.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
+import localStorage from '../../utils/localStorage';
 import LandingPage from '../LandingPage/LandingPage';
+import LoginPage from '../LoginPage/LoginPage';
 
 class CheckAuthentication extends React.Component {
   render() {
+    const accessToken = localStorage.get('spotify_access_token');
+    const refreshToken = localStorage.get('spotify_refresh_token');
+    if (!accessToken || !refreshToken) {
+      return (
+        <LoginPage />
+      );
+    }
     return (
       <>
-        <a href="/login">go to login</a>
         <Route path="/" exact component={LandingPage} />
       </>
     );
