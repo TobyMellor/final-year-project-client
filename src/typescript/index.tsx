0ab6e1e3ed9a4eec52b57ffc6e67d70f5d0615ba
Vakar Eeralil <varghese@m5nv.com>
2019-05-19 13:54:18 +0200

    connecting to store

--- src/typescript/index.tsx
+++ src/typescript/index.tsx
@@ -3,9 +3,13 @@ import '../sass/style.scss';
 
 import * as React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRoutes from './routes';
+import store from './store';
 
 render(
-    <AppRoutes />,
+    <Provider  store={store}>
+        <AppRoutes />
+    </Provider>,
     document.getElementById('root'),
 );
