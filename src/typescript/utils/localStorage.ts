5c00fdd083f008bc1961ef9dd6d95da41c43a000
Vakar Eeralil <varghese@m5nv.com>
2019-05-21 07:53:02 +0200

    consistent code. change object to methods

--- src/typescript/utils/localStorage.ts
+++ src/typescript/utils/localStorage.ts
@@ -1,26 +1,27 @@
-export default {
-  get: (key: string) => {
-    try {
-      const item: any = localStorage.getItem(key);
-      return JSON.parse(item);
-    } catch (e) {
-      return false;
-    }
-  },
-  set: (key: string, value: any) => {
-    try {
-      const stringValue = JSON.stringify(value);
-      localStorage.setItem(key, stringValue);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  },
-  remove: (key: string) => {
-    if (!localStorage.getItem(key)) {
-      return false;
-    }
-    localStorage.removeItem(key);
+
+export const get = (key: string) => {
+  try {
+    const item: any = localStorage.getItem(key);
+    return JSON.parse(item);
+  } catch (e) {
+    return false;
+  }
+};
+
+export const set = (key: string, value: any) => {
+  try {
+    const stringValue = JSON.stringify(value);
+    localStorage.setItem(key, stringValue);
     return true;
-  },
+  } catch (e) {
+    return false;
+  }
+};
+
+export const remove = (key: string) => {
+  if (!localStorage.getItem(key)) {
+    return false;
+  }
+  localStorage.removeItem(key);
+  return true;
 };
