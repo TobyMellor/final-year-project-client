9433c38bb3058bf5de8e38d5dfa055d2916988ec
Vakar Eeralil <varghese@m5nv.com>
2019-05-29 08:37:48 +0200

    Adding a search field

--- src/typescript/containers/search-page/SearchPage.tsx
+++ src/typescript/containers/search-page/SearchPage.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import * as React from 'react';
 
 class SearchPage extends React.Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <div className="container">
+          <div className="row align-items-center justify-content-center search-page">
+            <div className="col col-md-8 col-lg-6 align-self-center">
+              <input className="form-control form-control-lg" type="text" placeholder="Search Track" />
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
