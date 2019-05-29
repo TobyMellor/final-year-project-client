37be81347bf5ab12a8bc80df617009e6d46c2de8
Vakar Eeralil <varghese@m5nv.com>
2019-05-29 12:24:21 +0200

    search page connected to search results

--- src/typescript/containers/search-page/SearchPage.tsx
+++ src/typescript/containers/search-page/SearchPage.tsx
@@ -1,13 +1,31 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import debounce from 'lodash-es/debounce';
+import { searchSpotifyTrack } from '../../actions/search-actions';
+import { CombinedState } from '../../types/redux-state';
 
 class SearchPage extends React.Component {
+  constructor(props: null) {
+    super(props);
+    this.handleSearchChange = this.handleSearchChange.bind(this);
+  }
+
+  handleSearchChange(event: React.FormEvent) {
+    debounce(searchSpotifyTrack);
+  }
+
   render() {
     return (
       <div>
         <div className="container">
           <div className="row align-items-center justify-content-center search-page">
             <div className="col col-md-8 col-lg-6 align-self-center">
-              <input className="form-control form-control-lg" type="text" placeholder="Search Track" />
+              <input
+                className="form-control form-control-lg"
+                type="text"
+                placeholder="Search Track"
+                onChange={this.handleSearchChange}
+              />
             </div>
           </div>
         </div>
@@ -16,4 +34,10 @@ class SearchPage extends React.Component {
   }
 }
 
-export default SearchPage;
+const mapStateToProps = (state:CombinedState) => {
+  return {
+    searchResult: state.search.tracks,
+  };
+};
+
+export default connect(mapStateToProps, null)(SearchPage);
