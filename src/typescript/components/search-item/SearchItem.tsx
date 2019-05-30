704e04055f61310923b439dd249eaade298a7819
Vakar Eeralil <varghese@m5nv.com>
2019-05-30 11:33:03 +0200

    Sample search component with static values

--- src/typescript/components/search-item/SearchItem.tsx
+++ src/typescript/components/search-item/SearchItem.tsx
@@ -8,7 +8,20 @@ interface SearchItemProps {
 
 const SearchItem: React.FC<SearchItemProps> = (props: SearchItemProps) => (
   <div className="search-item">
-    hello world
+      <div className="thumbnail">
+        <img
+          src="https://cdn3.iconfinder.com/data/icons/website-panel-icons/128/test1-10-512.png"
+          alt="thumbnail"
+          />
+      </div>
+      <div className="info">
+        <div className="title">
+          Sample title
+        </div>
+        <div className="duration">
+          Duration: 12min
+        </div>
+      </div>
   </div>
 );
 
