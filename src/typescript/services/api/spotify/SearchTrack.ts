c35fe0da97add92e7977dc2cf3b8a79e910f07cf
Vakar Eeralil <varghese@m5nv.com>
2019-06-03 12:11:45 +0200

    Search track api implementation

--- src/typescript/services/api/spotify/SearchTrack.ts
+++ src/typescript/services/api/spotify/SearchTrack.ts
@@ -1,37 +1,60 @@
-import Request from '../Request';
-// import { objectToQueryParams } from '../../../utils/conversions';
 import SpotifyAPI from './SpotifyAPI';
+import Request from '../Request';
+import { objectToQueryParams } from '../../../utils/conversions';
 import SearchTrackModel, { OutputTrack } from '../../../models/search-track';
-import { SearchTrackResponse, Track } from '../../../types/spotify-responses';
+import { SearchTrackResponseData, Track, SearchTrackResponse } from '../../../types/spotify-responses';
+import * as localStorage from '../../../utils/localStorage';
 
 class SearchTrack extends Request {
-
-  getEndpoint() {
-    return 'v1/search/';
+  private query: string;
+  constructor(query: string) {
+    super();
+    this.query = query;
+    this.baseURL = 'https://api.spotify.com/v1';
   }
 
-  static async request(id: string): Promise<OutputTrack[]> {
+  getEndpoint() {
+    return `${this.baseURL}/search?${this.queryParams}`;
+  }
+
+  get headers() {
+    return {
+      Authorization: `Bearer ${localStorage.get('spotify_access_token')}`,
+    };
+  }
+
+  static async request(query: string): Promise<OutputTrack[]> {
     const response = await SpotifyAPI.get(
-      new SearchTrack(),
-    );
-    const tracks: Track[] = (response as SearchTrackResponse).tracks.items;
-    return new SearchTrackModel(tracks).tracks;
+      new SearchTrack(query),
+    ) as SearchTrackResponse;
+    if (response.status === 200) {
+      const tracks: Track[] = response.data.tracks.items;
+      return new SearchTrackModel(tracks).tracks;
+    }
+    return null;
   }
 
   async mockResponse(): Promise<OutputTrack[]> {
-    const response: SearchTrackResponse = require('../mocks/spotify/search-track').searchTracksMock();
-    const tracks : Track[] = (response as SearchTrackResponse).tracks.items;
+    const response: SearchTrackResponseData = require('../mocks/spotify/search-track').searchTracksMock();
+    const tracks : Track[] = (response as SearchTrackResponseData).tracks.items;
     return new SearchTrackModel(tracks).tracks;
   }
 
-  public getParams() {
-    const params = {
+  get queryParams() {
+    const queryParamObject = {
+      q: this.query,
       type: 'track',
       market: 'from_token',
       limit: '20',
     };
+    return objectToQueryParams(queryParamObject);
+  }
+
+  public getParams() {
+    const params = {
+      headers: this.headers,
+    };
     return params;
-    // return objectToQueryParams(params);
   }
 }
 
