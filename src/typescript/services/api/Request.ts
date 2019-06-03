2eff6092af413cba933925ab0ee8dd5750503c17
Vakar Eeralil <varghese@m5nv.com>
2019-06-03 11:58:34 +0200

    Modifying request to accomodate query params and headers

--- src/typescript/services/api/Request.ts
+++ src/typescript/services/api/Request.ts
@@ -1,6 +1,15 @@
 abstract class Request {
+  protected baseURL: string;
   public abstract mockResponse(): Promise<Object>;
   public abstract getEndpoint(): string;
+
+  public get queryParams(): string {
+    return '';
+  }
+
+  public get headers(): object {
+    return {};
+  }
 
   public getParams(): Object {
     return {};
