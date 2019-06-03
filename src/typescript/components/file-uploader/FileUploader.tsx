dd7238ab11004e5e4a6c7fe228fff3ee91200d60
Vakar Eeralil <varghese@m5nv.com>
2019-06-03 18:43:36 +0200

    Fixing the types and accpeting only audio type

--- src/typescript/components/file-uploader/FileUploader.tsx
+++ src/typescript/components/file-uploader/FileUploader.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 type FileUploaderProps = {
-  handleFileChange: () => void;
+  handleFileChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   disabled: boolean;
 };
 
 const FileUploader = (props: FileUploaderProps) => (
   <button className="btn btn-outline-primary file-uploader" disabled={props.disabled}>
     Upload Track
-    <input type="file" id="upload-track" onChange={props.handleFileChange} disabled={props.disabled} />
+    <input
+      type="file"
+      id="upload-track"
+      onChange={props.handleFileChange}
+      disabled={props.disabled}
+      accept=".mp3,audio/*"
+    />
   </button>
 );
 
