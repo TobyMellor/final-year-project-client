9a07a6737822748a962ca003252fa6edc1aa25e8
Vakar Eeralil <varghese@m5nv.com>
2019-06-03 13:44:33 +0200

    integrating onClick and active class

--- src/typescript/components/search-item/SearchItem.tsx
+++ src/typescript/components/search-item/SearchItem.tsx
@@ -2,28 +2,39 @@ import * as React from 'react';
 import { msToMinutes } from '../../utils/misc';
 
 interface SearchItemProps {
+  id: string;
   name: string;
   thumbnail: string;
   duration: string;
+  handleClick: (id: string) => void;
+  active: boolean;
 }
 
-const SearchItem: React.FC<SearchItemProps> = (props: SearchItemProps) => (
-  <div className="search-item">
-      <div className="thumbnail">
-        <img
-          src={props.thumbnail}
-          alt="thumbnail"
-          />
+const SearchItem: React.FC<SearchItemProps> = (props: SearchItemProps) => {
+  const onItemClick = () => {
+    props.handleClick(props.id);
+  };
+
+  return (
+    (
+      <div className={`search-item ${props.active ? 'active' : ''}`} onClick={onItemClick}>
+          <div className="thumbnail">
+            <img
+              src={props.thumbnail}
+              alt="thumbnail"
+              />
+          </div>
+          <div className="info">
+            <div className="title">
+              {props.name}
+            </div>
+            <div className="duration">
+              Duration: {msToMinutes(parseInt(props.duration, 10))}
+            </div>
+          </div>
       </div>
-      <div className="info">
-        <div className="title">
-          {props.name}
-        </div>
-        <div className="duration">
-          Duration: {msToMinutes(parseInt(props.duration, 10))}
-        </div>
-      </div>
-  </div>
-);
+    )
+  )
+};
 
 export default SearchItem;
