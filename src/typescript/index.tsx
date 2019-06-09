56188f5ab96e659f1f1be8b8574a7af9356b3d5c
Toby Mellor <me@tobymellor.com>
2019-06-09 18:45:49 +0100

    fix: urls

--- src/typescript/index.tsx
+++ src/typescript/index.tsx
@@ -10,7 +10,7 @@ import AppRoutes from './routes';
 import store from './store';
 
 render(
-  <Provider  store={store}>
+  <Provider store={store}>
     <AppRoutes />
   </Provider>,
   document.getElementById('root'),
--- webpack.config.js
+++ webpack.config.js
@@ -72,16 +72,4 @@ module.exports = {
 		// new HtmlWebpackHarddiskPlugin(),
 		new HardSourceWebpackPlugin(),
 	],
-	optimization: {
-		runtimeChunk: 'single',
-		splitChunks: {
-			cacheGroups: {
-				vendor: {
-					test: /[\\/]node_modules[\\/]/,
-					name: 'vendors',
-					chunks: 'all',
-				},
-			},
-		},
-	},
 };
